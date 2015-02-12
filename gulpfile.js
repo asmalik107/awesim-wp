4cdcc78f4905d8aab737e71b981f0db8ad893854
Asim <asmalik@hotmail.com>
2015-02-12 13:23:13 +0000

    Add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,29 @@
-gulpfile.js
+'use strict';
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var sass = require('gulp-ruby-sass');
+
+gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
+
+gulp.task('sass', function() {
+    return sass('sass/style.scss',
+        {
+	      style: 'expanded',
+	      precision: 10
+	    })
+    	.on('error', function (err) {
+      		console.error('Error', err.message);
+   		})
+   		.pipe($.autoprefixer({
+      		browsers: ['last 1 version']
+    	}))
+        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest(''));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('sass/*.scss', ['sass']);
+});
+
+gulp.task('default', ['clean', 'sass', 'watch']);
