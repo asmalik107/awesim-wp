a875c59f816f18eba76bfc55832cd60a608f0d12
Asim <asmalik@hotmail.com>
2016-01-25 17:00:12 +0000

    change search bar

--- js/navigation.js
+++ js/navigation.js
@@ -4,20 +4,27 @@
  * Handles toggling the navigation menu for small screens and enables tab
  * support for dropdown menus.
  */
-( function() {
-	var mobileMenu = $('.mobile-menu');
+(function () {
+    var mobileMenu = $('.mobile-menu');
     var toggleButton = $('.button-toggle');
-	var searchBar = $('.search-bar');
-	var toggleSearch = $('.search-click');
+    var searchBar = $('.search-bar');
+    var toggleSearch = $('.search-click');
+    var searchField = $('.search-field');
 
-    toggleButton.click(function(){
+    toggleButton.click(function () {
         mobileMenu.toggleClass('mobile-menu-open');
     });
 
 
-	toggleSearch.click(function(){
-		searchBar.toggleClass('search-bar-open');
-	});
+    toggleSearch.click(function () {
+        searchBar.toggleClass('search-bar-open');
+
+        if (searchBar.hasClass('search-bar-open')) {
+            searchField.focus();
+        } else {
+            searchField.blur();
+        }
+    });
 
 
-} )();
+})();
